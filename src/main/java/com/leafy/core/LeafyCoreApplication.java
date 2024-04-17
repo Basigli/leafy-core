add3a286821f9d58239e64f52fb6e12f0ace9c60
Basigli <gabrielebasigli@gmail.com>
2024-04-17 17:37:18 +0200

    Initial commit

---
+++ src/main/java/com/leafy/core/LeafyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.leafy.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafyCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leafy/core/LeafyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leafy.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafyCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
