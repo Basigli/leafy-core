e355b5435b7a680aac3a351a17bfa6cb519f30c7
Gabriele Basigli <84378604+Basigli@users.noreply.github.com>
2025-04-04 18:21:46 +0200

    (refactor) sensor: new sensor classes

---
+++ src/main/java/com/leafy/core/model/greenhouse/sensors/LightQuantitySensor.java
@@ -0,0 +1,12 @@
+package com.leafy.core.model.greenhouse.sensors;
+
+public class LightQuantitySensor extends Sensor {
+    public LightQuantitySensor(String arduinoId) {
+        super(arduinoId);
+    }
+
+    @Override
+    public Float readValue() {
+        return null;
+    }
+}
---
+++ src/main/java/com/leafy/core/model/greenhouse/sensors/TerrainHumiditySensor.java
@@ -0,0 +1,13 @@
+package com.leafy.core.model.greenhouse.sensors;
+
+public class TerrainHumiditySensor extends Sensor {
+
+    public TerrainHumiditySensor(String arduinoId) {
+        super(arduinoId);
+    }
+
+    @Override
+    public Float readValue() {
+        return null;
+    }
+}
